6cbd7cfaa279da8ba944b1b95bad9ab7ec8202fa
le4535441-star <le4535441-star@users.noreply.github.com>
2026-06-11 00:25:16 +0800

    标签「好喝的咖啡」改为「好喝的」，升级SW缓存v21

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'happy-diary-cloud-v20';
+const CACHE_NAME = 'happy-diary-cloud-v21';
 const ASSETS = ['./index.html', './manifest.json', './sign-data.js'];
 
 self.addEventListener('install', e => {
