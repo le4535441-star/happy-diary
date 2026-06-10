2627a6a1aee6550501e029967c278ceb50956f71
le4535441-star <le4535441-star@users.noreply.github.com>
2026-06-10 10:52:28 +0800

    first commit

---
+++ sw.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = 'happy-diary-cloud-v1';
+const ASSETS = ['/index.html', '/manifest.json'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+    )).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  // 网络优先，失败用缓存
+  e.respondWith(
+    fetch(e.request).then(resp => {
+      const clone = resp.clone();
+      caches.open(CACHE_NAME).then(cache => cache.put(e.request, clone));
+      return resp;
+    }).catch(() => caches.match(e.request).then(c => c || caches.match('/index.html')))
+  );
+});
