fa25d115ab3c4b390d10d062ed65da8d63978a2c
le4535441-star <le4535441-star@users.noreply.github.com>
2026-06-10 14:18:41 +0800

    修复：求签结果签级完全从signFullData获取，根治'都显示中签'的bug

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'happy-diary-cloud-v2';
+const CACHE_NAME = 'happy-diary-cloud-v3';
 const ASSETS = ['./index.html', './manifest.json', './sign-data.js'];
 
 self.addEventListener('install', e => {
