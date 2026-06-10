9198cdeedc781fc7b764d881573b3887cd98c2f9
le4535441-star <le4535441-star@users.noreply.github.com>
2026-06-11 00:29:00 +0800

    闪屏时长3秒→5秒，升级SW缓存v22

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'happy-diary-cloud-v21';
+const CACHE_NAME = 'happy-diary-cloud-v22';
 const ASSETS = ['./index.html', './manifest.json', './sign-data.js'];
 
 self.addEventListener('install', e => {
