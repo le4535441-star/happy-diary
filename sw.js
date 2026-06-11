aa6193502b7684bcb3ce69e597fff54084a66cae
le4535441-star <le4535441-star@users.noreply.github.com>
2026-06-11 15:59:02 +0800

    小女孩：帽子/绒球/背带裙/帽檐线全部设为透明，只保留头部轮廓和白色短袖，SW升v75

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'happy-diary-cloud-v74';
+const CACHE_NAME = 'happy-diary-cloud-v75';
 const ASSETS = ['./index.html', './manifest.json', './sign-data.js'];
 
 self.addEventListener('install', e => {
