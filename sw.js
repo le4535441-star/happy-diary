7be909924edd86cbfce577e0e3fcbb2d42e644e3
le4535441-star <le4535441-star@users.noreply.github.com>
2026-06-11 14:17:57 +0800

    生成卡片：截图前同时隐藏天气和背景图(bg-image/card-bg)，解决用户上传图片导致的canvas taint，SW升v60

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'happy-diary-cloud-v59';
+const CACHE_NAME = 'happy-diary-cloud-v60';
 const ASSETS = ['./index.html', './manifest.json', './sign-data.js'];
 
 self.addEventListener('install', e => {
