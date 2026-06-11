b24475bacb15e8ee6f819191e0287a4235385dc7
le4535441-star <le4535441-star@users.noreply.github.com>
2026-06-11 13:45:37 +0800

    还原生成卡片到最佳版本(commit 3528c68)：纯html2canvas+devicePixelRatio高清截图，去掉降级手绘，SW升v55

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'happy-diary-cloud-v54';
+const CACHE_NAME = 'happy-diary-cloud-v55';
 const ASSETS = ['./index.html', './manifest.json', './sign-data.js'];
 
 self.addEventListener('install', e => {
