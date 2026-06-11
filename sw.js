278ae5718e4b236abe3935775935ab716afa6b9f
le4535441-star <le4535441-star@users.noreply.github.com>
2026-06-11 17:58:07 +0800

    彻底隐藏第二个claw-btn(旧游戏模式里的)，右上角圆球才是真正按钮，SW升v97

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'happy-diary-cloud-v96';
+const CACHE_NAME = 'happy-diary-cloud-v97';
 const ASSETS = ['./index.html', './manifest.json', './sign-data.js'];
 
 self.addEventListener('install', e => {
