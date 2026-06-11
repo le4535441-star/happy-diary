a0d1e5bda6110e18faf1445d03938fc8676e7ee7
le4535441-star <le4535441-star@users.noreply.github.com>
2026-06-11 17:38:28 +0800

    去掉圆球按钮的title属性，不再弹出iOS tooltip文字，SW升v94

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'happy-diary-cloud-v93';
+const CACHE_NAME = 'happy-diary-cloud-v94';
 const ASSETS = ['./index.html', './manifest.json', './sign-data.js'];
 
 self.addEventListener('install', e => {
