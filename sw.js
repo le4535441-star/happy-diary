fb7a2746badbf0d885394bb7a034820b81ebe9cf
le4535441-star <le4535441-star@users.noreply.github.com>
2026-06-11 16:03:46 +0800

    还原小女孩原版SVG，SW升v76

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'happy-diary-cloud-v75';
+const CACHE_NAME = 'happy-diary-cloud-v76';
 const ASSETS = ['./index.html', './manifest.json', './sign-data.js'];
 
 self.addEventListener('install', e => {
