52c66233030d9398b97fa7493b88f12252dffe40
le4535441-star <le4535441-star@users.noreply.github.com>
2026-06-11 14:02:58 +0800

    根治生成卡片insecure：截图前清空weather-display内容避免跨域污染，截图后还原，SW升v58

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'happy-diary-cloud-v57';
+const CACHE_NAME = 'happy-diary-cloud-v58';
 const ASSETS = ['./index.html', './manifest.json', './sign-data.js'];
 
 self.addEventListener('install', e => {
