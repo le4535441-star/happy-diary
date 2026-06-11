b0ef86088b07bbd07b084766dbde3c37a8cc46a8
le4535441-star <le4535441-star@users.noreply.github.com>
2026-06-11 17:05:05 +0800

    哈哈机：flex布局填满高度，游戏框flex:1自动撑高，按钮在下方，SW升v89

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'happy-diary-cloud-v88';
+const CACHE_NAME = 'happy-diary-cloud-v89';
 const ASSETS = ['./index.html', './manifest.json', './sign-data.js'];
 
 self.addEventListener('install', e => {
