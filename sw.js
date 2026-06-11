0f3d78c84828dcd378317dbd6538822552a709b9
le4535441-star <le4535441-star@users.noreply.github.com>
2026-06-11 17:19:20 +0800

    笑话弹出页z-index调高至150盖住圆球按钮(z-index:100)，接球中圆球可见，笑话时被遮住，SW升v93

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'happy-diary-cloud-v92';
+const CACHE_NAME = 'happy-diary-cloud-v93';
 const ASSETS = ['./index.html', './manifest.json', './sign-data.js'];
 
 self.addEventListener('install', e => {
