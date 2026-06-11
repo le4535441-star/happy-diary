588b1731c0f43a25e60e58052bf28611db686b42
le4535441-star <le4535441-star@users.noreply.github.com>
2026-06-11 10:05:27 +0800

    同步屏改为闪屏同风格（暖色渐变+emoji+温柔文案），去掉冷冰冰的loading转圈，SW升v32

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'happy-diary-cloud-v31';
+const CACHE_NAME = 'happy-diary-cloud-v32';
 const ASSETS = ['./index.html', './manifest.json', './sign-data.js'];
 
 self.addEventListener('install', e => {
