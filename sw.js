e5a368db5a83b956c1d79a811b577d93b55ed48b
le4535441-star <le4535441-star@users.noreply.github.com>
2026-06-11 13:51:08 +0800

    修复生成卡片insecure报错：allowTaint:false+忽略weather-display+imageTimeout:0，SW升v57

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'happy-diary-cloud-v56';
+const CACHE_NAME = 'happy-diary-cloud-v57';
 const ASSETS = ['./index.html', './manifest.json', './sign-data.js'];
 
 self.addEventListener('install', e => {
