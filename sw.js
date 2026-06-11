89af769f2e0b42f0c90da92138ca8975b045a912
le4535441-star <le4535441-star@users.noreply.github.com>
2026-06-11 16:14:08 +0800

    哈哈机：游戏框上/左/右贴屏幕边，底部保留圆角和边框，SW升v79

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'happy-diary-cloud-v78';
+const CACHE_NAME = 'happy-diary-cloud-v79';
 const ASSETS = ['./index.html', './manifest.json', './sign-data.js'];
 
 self.addEventListener('install', e => {
