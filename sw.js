c71909d298d3c22611eaf81828ef4cf8e0d8251e
le4535441-star <le4535441-star@users.noreply.github.com>
2026-06-11 12:44:52 +0800

    观音提示去句号；签文清缓存恢复：syncScreen/SyncAndRefresh两条路径都调updateSignCount，SW升v44

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'happy-diary-cloud-v43';
+const CACHE_NAME = 'happy-diary-cloud-v44';
 const ASSETS = ['./index.html', './manifest.json', './sign-data.js'];
 
 self.addEventListener('install', e => {
