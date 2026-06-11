03e12adb82172348a49bd55245e8db840cb49d60
le4535441-star <le4535441-star@users.noreply.github.com>
2026-06-11 14:11:49 +0800

    生成卡片：截图前用display:none隐藏天气区域(非innerHTML清空)，useCORS:false，SW升v59

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'happy-diary-cloud-v58';
+const CACHE_NAME = 'happy-diary-cloud-v59';
 const ASSETS = ['./index.html', './manifest.json', './sign-data.js'];
 
 self.addEventListener('install', e => {
