5feaf6879ca23f173b007fa653c40b5671240384
le4535441-star <le4535441-star@users.noreply.github.com>
2026-06-11 09:55:22 +0800

    根治数据丢失：ensureShredStore/ensureBubblePoolStore不再重新open IndexedDB覆盖db变量；卡片正文左对齐金句居中；闪屏金句居中；SW升v29

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'happy-diary-cloud-v28';
+const CACHE_NAME = 'happy-diary-cloud-v29';
 const ASSETS = ['./index.html', './manifest.json', './sign-data.js'];
 
 self.addEventListener('install', e => {
