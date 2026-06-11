b90e257a0c0d58f5464b725d8b0e56c9dab5fa42
le4535441-star <le4535441-star@users.noreply.github.com>
2026-06-11 16:08:17 +0800

    哈哈机：游戏框max-height扩至620px，按钮间距缩紧贴近tab-bar，SW升v77

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'happy-diary-cloud-v76';
+const CACHE_NAME = 'happy-diary-cloud-v77';
 const ASSETS = ['./index.html', './manifest.json', './sign-data.js'];
 
 self.addEventListener('install', e => {
