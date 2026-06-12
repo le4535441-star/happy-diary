7c0e6e6683b4b1c011028e972dd66635f9c04d30
le4535441-star <le4535441-star@users.noreply.github.com>
2026-06-12 12:46:38 +0800

    根本修复SecurityError：移除diary-page::before的SVG filter纹理(feTurbulence)，iOS html2canvas不再taint canvas，SW升v112

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'happy-diary-cloud-v111';
+const CACHE_NAME = 'happy-diary-cloud-v112';
 const ASSETS = ['./index.html', './manifest.json', './sign-data.js'];
 
 self.addEventListener('install', e => {
