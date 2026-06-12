baca22549366f31257abf87a4d01ac52884ba0eb
le4535441-star <le4535441-star@users.noreply.github.com>
2026-06-12 10:34:22 +0800

    闪屏后默认进入记好事；修复tab-laugh/tab-happy div id错误；默认active改到tab-happy，SW升v106

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'happy-diary-cloud-v105';
+const CACHE_NAME = 'happy-diary-cloud-v106';
 const ASSETS = ['./index.html', './manifest.json', './sign-data.js'];
 
 self.addEventListener('install', e => {
