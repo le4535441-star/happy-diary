8b2ac7b4425d86da1754d6c655a271ef78513486
le4535441-star <le4535441-star@users.noreply.github.com>
2026-06-12 15:00:37 +0800

    闪屏：配文下方加摇摆小铃铛🔔+文字提示，引导触摸激活音效，SW升v132

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'happy-diary-cloud-v131';
+const CACHE_NAME = 'happy-diary-cloud-v132';
 const ASSETS = ['./index.html', './manifest.json', './sign-data.js'];
 
 self.addEventListener('install', e => {
